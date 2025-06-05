bce76d97d647254f5fd37fc214704d763dc3a02b
JuvitaSaini09 <juvitasaini@gmail.com>
2025-06-05 22:31:13 +0530

    initial commit

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "DevHistory",
+  keywords:
+    "DevHistory, career history, developer portfolio, tech career, programming journey, professional growth, skills documentation, career milestones, developer achievements, career tracking, software engineering history, tech industry,, career documenttion, personal development, career reflection, software projects, coding journey, developer stories",
+  description:
+    "DevHistory is a platform dedicated to documenting the history of your career in software development or any other field of interest.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={``}>{children}</body>
+    </html>
+  );
+}
